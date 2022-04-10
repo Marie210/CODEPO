d5b9dee3e1734511d134e0978b11d2c8078a29b7
colinDietrich <44623378+colinDietrich@users.noreply.github.com>
2022-04-10 21:03:45 +0200

    Update thingstream.ino

--- codes_arduino/main/thingstream.ino
+++ codes_arduino/main/thingstream.ino
@@ -1,54 +1,33 @@
-/**
-ensure that the 4 initialization steps needed to connect the click and the thingstream server are done 
-**/
-
 void initThingstream(int *flag_init) {
-  Serial.println("INITIALISATION");
-  
-  //DEBUG allows the clicks to get clear error messages
   if (*flag_init == 0 ){
+    // reset the Thingstream click
+    digitalWrite(3, LOW);
+    delay(1200);
+    digitalWrite(3, HIGH);
     Serial.println("DEBUG");
     Serial1.println("AT+IOTDEBUG=0");
-    delay(5000);
     
     if(checkReception() == 1) { *flag_init = 1; }
-    Serial.println(*flag_init);
   }
-  
-  //CREATE create the connection between the click and the server
   else if(*flag_init == 1){
     Serial.println("CREATE");
     Serial1.println("AT+IOTCREATE");  
-    delay(5000); 
     
     if(checkReception() == 1) { *flag_init = 2; }
-    Serial.println(*flag_init);
   }
-  
-  //CONNECT is the second step of the connection between the click and the server
   else if(*flag_init == 2){
     Serial.println("CONNECT");
     Serial1.println("AT+IOTCONNECT=true"); 
-    delay(5000);
      
     if(checkReception() == 1) { *flag_init = 3; }
-    Serial.println(*flag_init);
   }
-  
-  //SUBSCRIBE to a topic allows the click to receive the message published in this topic
   else if(*flag_init == 3) {
     Serial.println("SUBSCRIBE");
     Serial1.println("AT+IOTSUBSCRIBE=\"TEST1\",1");
-    delay(5000);
     
     if(checkReception() == 1) { *flag_init = 4; }
-    Serial.println(*flag_init);
   }
 }
-
-/**
-ensure that the message send by the click has been received by the server
-**/
 
 int checkReception() {
   bool check = true;
@@ -57,39 +36,75 @@ int checkReception() {
   int count = 0;
   
   while(check) {
-
-    /*
-    double Vt_Actual = 0.0, U = 0.0;
-    measureVoltage(&Vt_Actual, &U);
-    Serial.print(Vt_Actual, 5); Serial.print("  VT;  ");
-    Serial.print(U, 5); Serial.print("  U;  ");
-    Serial.print("\n");
-    */
     
     if(millis()*0.001 - timeInit > 20.0) { 
-      Serial.println("To Long Time");  
+      Serial.println("To Long Time");
+      check = false;  
       return 0;
      }
 
-     if (Serial1.available ()){
+     if (Serial1.available()){
         char st = Serial1.read();
-        count += 1; 
         message[count] = st;  
         message[count+1] = '\0';  
+        count += 1; 
                 
-        if(message[count] == '\n'){
+        if(message[count-1] == '\n'){
           Serial.print(String(message));  
           if (analyse(message) ==  1) {
-            Serial.println("SUCCESS");  
+            check = false;
             return 1;
           } else {
-            Serial.println("FAILURE");
+            check = false;
             return 0;
           }
         }
      }
   }
   
+}
+
+int readCommand(String *res) {
+  bool check = true, start = false;
+  unsigned long timeInit = millis()*0.001;
+  char message[150];
+  int count = 0;
+ 
+  while(check) {
+    
+    if(millis()*0.001 - timeInit > 20.0) { 
+      Serial.println("To Long Time to read command");  
+      return 0;
+     }
+    
+     if (Serial1.available()) {
+        char st = Serial1.read();
+        
+        if(start == false) {
+          start = true;
+          if(st != '*') {
+            check = false;
+            return 0;
+          }
+        } else {
+            if(st == '*') {
+                *res = String(message);
+                Serial.print("reception message : ");
+                Serial.print(*res);  
+                Serial.print("first letter : ");
+                Serial.println(message[0]);
+                return 1;
+            } else {
+                message[count] = st;  
+                message[count+1] = '\0';  
+                count += 1; 
+            }
+        }
+     } else if(start == false) {
+          check = false;
+          return 0;
+     }
+  }
 }
 
 
@@ -104,22 +119,93 @@ int analyse (String st) {
   return res; 
 }
 
-/**
-Publish a message on a topic with the Thingstream click
-**/
-int publish(double Vt_Actual, double U, double X[3], double Z[5], double error){
+int publish(double Vt_Actual, double U, double X[3], double Z[5], double error, double temperature){
 
     char message[150];
-    sprintf(message, "AT+IOTPUBLISH=\"TEST1\",1,\"{'tension': %d, 'courant': %d, 'Error': %d, 'SOC': %d}\"", Vt_Actual, U, error, X[0]);
+    sprintf(message, "AT+IOTPUBLISH=\"TEST1\",1,\"{'tension': %f, 'courant': %f, 'Error': %f, 'SOC': %f, 'temperature' : %f}\"", Vt_Actual, U, error, X[0], temperature);
+
     
-    // ask the Thingstream click to publish the message
+    // envoie du message vers le Thingstream click
     Serial1.println(message);
     
-    // check that the message has been received
+    // verification si le message a bien été réceptionné ou non
     int flag = checkReception();
     if(flag == 1) {
       Serial.println("SUCCSESS -- envoie du message :");
+      Serial.println(message);
     } else {
       Serial.println("FAIL -- envoie du message :");
+      Serial.println(message);
     }
+}
+
+void(* resetFunc) (void) = 0;
+
+void convertMessage(String input, bool *on, int *mode, double X[3], double Z[5], 
+              double SOCOCV[12], double dSOCOCV[11], double P_x[9], double P_z[25], double Q_x[9],
+              double Q_z[25], double *R_x, double *R_z, double *Qn_rated, double *current_rated, double *voltage_rated) {
+
+  // Allocate a temporary JsonDocument
+  // Don't forget to change the capacity to match your requirements.
+  StaticJsonDocument<3500> doc;
+  //input.replace("'", "\"");
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, String(input));
+  if (error) {
+    Serial.println(F("Failed to read file, using default configuration"));
+    Serial.println(error.f_str());
+  }
+
+  String option = doc["option"];
+  Serial.print("option = ");Serial.println(option);
+
+  if(option =="A") {
+      String val = doc["value"];
+      if(val == "on") { *on = true; }
+      if(val == "pause") { *on = false; }
+  } else if(option == "B") {
+      resetFunc();
+  } else if(option == "C") {
+    if(*on != true) {
+      *mode = doc["value"];
+    }
+  } else if(option == "D") {
+      X[0] = doc["SOC_init"];
+      *R_x = doc["R_x"];
+      *R_z = doc["R_z"];
+      *Qn_rated = doc["Qn_rated"];
+      *voltage_rated = doc["voltage_rated"];
+      *current_rated = doc["current_rated"];
+
+      int R0 = doc["R0"]; int R1 = doc["R1"]; int R2 = doc["R2"]; int C1 = doc["C1"]; int C2 = doc["C2"]; 
+      Z[0] = R0; Z[1] = R1; Z[2] = R2;
+      Z[3] = R1 * C1;
+      Z[4] = R2 * C2;
+      
+      for (int i = 0; i < 12; i++) {
+        SOCOCV[i] = doc["SOCOCV"][i];
+      }
+    
+      for (int i = 0; i < 11; i++) {
+        if(i%8 == 0) {
+          P_x[i] = doc["P_x"];
+          Q_x[i] = doc["Q_x"];
+        } else {
+          P_x[i] = 0.0;
+          Q_x[i] = 0.0;
+        }
+        dSOCOCV[i] = doc["dSOCOCV"][i];
+      }
+    
+      for (int i = 0; i < 25; i++) {
+        if(i%6 == 0) {
+          P_z[i] = doc["P_z"];
+          Q_z[i] = doc["Q_z"];
+        } else {
+          P_z[i] = 0.0;
+          Q_z[i] = 0.0;
+        }
+      }
+  }
+  
 }
