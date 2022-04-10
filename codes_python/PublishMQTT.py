e5cbf710b6f7899a547d7ff07b613e7be589cc89
colinDietrich <44623378+colinDietrich@users.noreply.github.com>
2022-04-10 20:59:39 +0200

    Update PublishMQTT.py

--- codes_python/PublishMQTT.py
+++ codes_python/PublishMQTT.py
@@ -7,8 +7,8 @@ clientid = "device:d4574f32-d632-4501-8d5b-b8c9e801b5e5"
 username = "K7T841RL7HZ3P98IBS6J"
 password = "Xrs5FIZ0MdHKaVHEmFw35kE8UTrR0qI3yk1ui6h3"
 
-def on_connect(client, userdata, flags, rc):    
-    print("Connected with result code "+str(rc))    
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
         
 client = mqtt.Client(client_id=clientid)
 client.username_pw_set(username,password)
@@ -16,7 +16,6 @@ client.username_pw_set(username,password)
 #Insecure connection on port 1883 !!!
 client.connect(host=hostname, port=1883, keepalive=60)
 id = 0
-while True:
-    client.publish("TEST1","STOP",1)
-    time.sleep(180)
-    id += 1
+
+message = "*{ \"option\": \"A\", \"value\": \"on\" }*"
+client.publish("TEST1",message,1)
