32fe3c9e31c4880df9e0504cac7fd331bd04c9f6
colinDietrich <44623378+colinDietrich@users.noreply.github.com>
2022-07-19 09:54:51 +0100

    Update initialisation.ino

--- codes_arduino/main/initialisation.ino
+++ codes_arduino/main/initialisation.ino
@@ -1,9 +1,9 @@
 #include <ArduinoJson.h>
 #include <SD.h>
 
-void initialisation(const String input, double X[3], double Z[5], 
-              double SOCOCV[12], double dSOCOCV[11], double P_x[9], double P_z[25], double Q_x[9],
-              double Q_z[25], double *R_x, double *R_z, double *Qn_rated, double *voltage_rated, double *current_rated) {
+void initialisation(const String input, double *X, double *Z, 
+              double SOCOCV[12], double dSOCOCV[11], double *P_x, double *P_z, double *Q_x,
+              double *Q_z, double *R_x, double *R_z, double *Qn_rated, double *voltage_rated, double *current_rated) {
               double parameters[5];
               double SOC_init;
               // load data from JSON file
@@ -17,8 +17,8 @@ void initialisation(const String input, double X[3], double Z[5],
 
 
 // Loads the configuration from a file
-void loadJson(String input, double parameters[5], double *SOC_init, double SOCOCV[12], double dSOCOCV[11], 
-              double P_x[9], double P_z[25], double Q_x[9], double Q_z[25],
+void loadJson(String input, double *parameters, double *SOC_init, double SOCOCV[12], double dSOCOCV[11], 
+              double *P_x, double *P_z, double *Q_x, double *Q_z,
               double *R_x, double *R_z, double *Qn_rated, double *voltage_rated, double *current_rated) {
   
   // Allocate a temporary JsonDocument
@@ -75,5 +75,57 @@ void loadJson(String input, double parameters[5], double *SOC_init, double SOCOC
       P_z[i] = 0;
       Q_z[i] = 0;
     }
+  }
+}
+
+void duplicate(double *X, double *Z, double *P_x, double *P_z, double *Q_x, 
+              double *Q_z, double *R_x, double *R_z, int nbBatteries) {
+                
+            for(int i = 1; i < nbBatteries; i++) {
+              for (int j = 0; j < 9; j++) {
+                Q_x[i*9 + j] = Q_x[j];
+                P_x[i*9 + j] = P_x[j];
+              }
+              for (int j = 0; j < 5; j++) {
+                Z[i*5 + j] = Z[j];
+              }
+              for (int j = 0; j < 3; j++) {
+                X[i*3 + j] = X[j];
+              }
+              for (int j = 0; j < 25; j++) {
+                P_z[i*25 + j] = P_z[j];
+                Q_z[i*25 + j] = Q_z[j];
+              }
+              R_x[i] = R_x[0];
+              R_z[i] = R_z[0];
+            }
+}
+
+void copy(double* src, double* dst, int len, int index1, int index2) {
+    for (int i = 0; i < len; i++) {
+        dst[index1+i] = src[index2+i];
+    }
+}
+
+void kalmanFilter(double I, double *X, double *Z, double *SOCOCV, double *dSOCOCV, double *V, double *P_x, double *P_z, double *Q_x, double *Q_z, double *R_x, double *R_z, double DeltaT, double Qn_rated, int nbBatteries) {
+  for(int i = 0; i < nbBatteries; i++) {
+    double Xb[3], Zb[5], P_xb[9], P_zb[25], Q_xb[9], Q_zb[25];
+    copy(X, Xb, 3, 0, i*3);
+    copy(Z, Zb, 5, 0, i*5);
+    copy(P_x, P_xb, 9, 0, i*9);
+    copy(P_z, P_zb, 25, 0, i*25);
+    copy(Q_x, Q_xb, 9, 0, i*9);
+    copy(Q_z, Q_zb, 25, 0, i*25);
+    double R_xb = R_x[i];
+    double R_zb = R_z[i];  
+    error = extendedKalmanFilter(I, Xb, Zb, SOCOCV, dSOCOCV, V[i], P_xb, P_zb, Q_xb, Q_zb, R_xb, R_zb, DeltaT, Qn_rated);
+    copy(Xb, X, 3, i*3, 0);
+    copy(Zb, Z, 5, i*5, 0);
+    copy(P_xb, P_x, 9, i*9, 0);
+    copy(P_zb, P_z, 25, i*25, 0);
+    copy(Q_xb, Q_x, 9, i*9, 0);
+    copy(Q_zb, Q_z, 25, i*25, 0);
+    R_x[i] = R_xb;
+    R_z[i] = R_zb;
   }
 }
