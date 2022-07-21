f6061bb88f56c32a2faa225c8f6c07f5c84cf0aa
colinDietrich <44623378+colinDietrich@users.noreply.github.com>
2022-07-21 14:45:03 +0100

    Update extendedKalmanFilter.cpp

--- codes_arduino/libraries/extended_kalman_filter/extendedKalmanFilter.cpp
+++ codes_arduino/libraries/extended_kalman_filter/extendedKalmanFilter.cpp
@@ -5,14 +5,13 @@
  * File: extendedKalmanFilter.c
  *
  * MATLAB Coder version            : 5.1
- * C/C++ source code generated on  : 15-Feb-2022 16:12:22
+ * C/C++ source code generated on  : 21-Jul-2022 14:32:40
  */
 
 /* Include Files */
 #include "extendedKalmanFilter.h"
 #include <math.h>
 #include <string.h>
-#include <Arduino.h>
 
 /* Function Definitions */
 /*
@@ -29,8 +28,8 @@
  * Arguments    : double U
  *                double X[3]
  *                double Z[5]
- *                const double SOCOCV[12]
- *                const double dSOCOCV[11]
+ *                const double SOCOCV[5]
+ *                const double dSOCOCV[4]
  *                double Vt_Actual
  *                double P_x[9]
  *                double P_z[25]
@@ -40,12 +39,14 @@
  *                double R_z
  *                double DeltaT
  *                double Qn_rated
- * Return Type  : double
+ *                double *Error_x
+ *                double *TerminalVoltage
+ * Return Type  : void
  */
-double extendedKalmanFilter(double U, double X[3], double Z[5], const double
-  SOCOCV[12], const double dSOCOCV[11], double Vt_Actual, double P_x[9], double
+void extendedKalmanFilter(double U, double X[3], double Z[5], const double
+  SOCOCV[5], const double dSOCOCV[4], double Vt_Actual, double P_x[9], double
   P_z[25], const double Q_x[9], const double Q_z[25], double R_x, double R_z,
-  double DeltaT, double Qn_rated)
+  double DeltaT, double Qn_rated, double *Error_x, double *TerminalVoltage)
 {
   double c_I[25];
   double d_I[25];
@@ -57,20 +58,18 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
   double C_x[3];
   double KalmanGain_x[3];
   double c_A[3];
-  double Error_x;
-  double OCV;
   double SOC;
   double a1_tmp;
   double a2_tmp;
   double d;
   double d1;
+  double d2;
   double dOCV;
+  int A_tmp;
   int i;
   int i1;
-  int k;
   signed char b_I[25];
-    
-    
+
   /*  States */
   SOC = X[0];
 
@@ -82,12 +81,13 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
   /*  Coef in B : Gk : Input control matrix */
   /*  Coef in mat : dXk/dThetak */
   /*  Linearizing the model by linearizing the C matrix */
-  dOCV = dSOCOCV[0];
-  for (k = 0; k < 10; k++) {
-    dOCV = SOC * dOCV + dSOCOCV[k + 1];
-  }
+  dOCV = SOC * (SOC * (SOC * dSOCOCV[0] + dSOCOCV[1]) + dSOCOCV[2]) + dSOCOCV[3];
 
   /*  dUoc(SOC)/dSOC at SOC = SOCk */
+  if (X[0] > 1.0) {
+    dOCV = 0.0;
+  }
+
   C_x[0] = dOCV;
   C_x[1] = -1.0;
   C_x[2] = -1.0;
@@ -119,17 +119,19 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
 
   /*  Htheta : Jacobian measurement matrix of theta */
   /*  Knowing SOC, we can compute OCV thanks to the relationship SOC-OCV */
-  OCV = SOCOCV[0];
-  for (k = 0; k < 11; k++) {
-    OCV = SOC * OCV + SOCOCV[k + 1];
-  }
+  SOC = SOC * (SOC * (SOC * (SOC * SOCOCV[0] + SOCOCV[1]) + SOCOCV[2]) + SOCOCV
+               [3]) + SOCOCV[4];
 
   /*  calculate the values of OCV at the given SOC, using the polynomial SOCOCV */
+  if (X[0] > 1.0) {
+    SOC = (((SOCOCV[0] + SOCOCV[1]) + SOCOCV[2]) + SOCOCV[3]) + SOCOCV[4];
+  }
+
   /*  Terminal voltage estimation */
+  *TerminalVoltage = ((SOC - Z[0] * U) - X[1]) - X[2];
+
   /*  Calculate the Vt error */
-  Error_x = Vt_Actual - (((OCV - Z[0] * U) - X[1]) - X[2]);
-    //Serial.print(" ---> C code -- V = ");
-    Serial.print((((OCV - Z[0] * U) - X[1]) - X[2]), 5); Serial.print("  CV;  ");
+  *Error_x = Vt_Actual - *TerminalVoltage;
 
   /*  --- EKF ALGORITHM --- */
   /*  --> SOC ESTIMATION */
@@ -149,65 +151,67 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
   for (i = 0; i < 3; i++) {
     i1 = (int)A[i];
     d = A[i + 3];
-    SOC = A[i + 6];
-    for (k = 0; k < 3; k++) {
-      b_A[i + 3 * k] = ((double)i1 * P_x[3 * k] + d * P_x[3 * k + 1]) + SOC *
-        P_x[3 * k + 2];
+    d1 = A[i + 6];
+    for (A_tmp = 0; A_tmp < 3; A_tmp++) {
+      b_A[i + 3 * A_tmp] = ((double)i1 * P_x[3 * A_tmp] + d * P_x[3 * A_tmp + 1])
+        + d1 * P_x[3 * A_tmp + 2];
     }
   }
 
   for (i = 0; i < 3; i++) {
     d = b_A[i];
-    SOC = b_A[i + 3];
-    d1 = b_A[i + 6];
+    d1 = b_A[i + 3];
+    d2 = b_A[i + 6];
     for (i1 = 0; i1 < 3; i1++) {
-      k = i + 3 * i1;
-      P_x[k] = ((d * A[i1] + SOC * A[i1 + 3]) + d1 * A[i1 + 6]) + Q_x[k];
+      A_tmp = i + 3 * i1;
+      P_x[A_tmp] = ((d * A[i1] + d1 * A[i1 + 3]) + d2 * A[i1 + 6]) + Q_x[A_tmp];
     }
   }
 
   /*  Update */
-  OCV = 0.0;
+  SOC = 0.0;
   for (i = 0; i < 3; i++) {
     KalmanGain_x[i] = (P_x[i] * dOCV + -P_x[i + 3]) + -P_x[i + 6];
-    OCV += ((dOCV * P_x[3 * i] + -P_x[3 * i + 1]) + -P_x[3 * i + 2]) * C_x[i];
+    SOC += ((dOCV * P_x[3 * i] + -P_x[3 * i + 1]) + -P_x[3 * i + 2]) * C_x[i];
   }
 
-  OCV = 1.0 / (OCV + R_x);
+  SOC = 1.0 / (SOC + R_x);
   for (i = 0; i < 3; i++) {
-    KalmanGain_x[i] *= OCV;
+    KalmanGain_x[i] *= SOC;
     c_A[i] = (A[i] * X[0] + A[i + 3] * X[1]) + A[i + 6] * X[2];
   }
 
   X[0] = -(DeltaT / Qn_rated) * U;
   X[1] = Z[1] * (1.0 - a1_tmp) * U;
   X[2] = Z[2] * (1.0 - a2_tmp) * U;
-  X[0] = (c_A[0] + X[0]) + KalmanGain_x[0] * Error_x;
-  X[1] = (c_A[1] + X[1]) + KalmanGain_x[1] * Error_x;
-  X[2] = (c_A[2] + X[2]) + KalmanGain_x[2] * Error_x;
+  X[0] = (c_A[0] + X[0]) + KalmanGain_x[0] * *Error_x;
+  X[1] = (c_A[1] + X[1]) + KalmanGain_x[1] * *Error_x;
+  X[2] = (c_A[2] + X[2]) + KalmanGain_x[2] * *Error_x;
+
+  /* X(1) = min(1, X(1)); */
   memset(&A[0], 0, 9U * sizeof(double));
   A[0] = 1.0;
   A[4] = 1.0;
   A[8] = 1.0;
   d = KalmanGain_x[0];
-  SOC = KalmanGain_x[1];
-  d1 = KalmanGain_x[2];
+  d1 = KalmanGain_x[1];
+  d2 = KalmanGain_x[2];
   for (i = 0; i < 3; i++) {
-    OCV = C_x[i];
-    b_A[3 * i] = A[3 * i] - d * OCV;
-    k = 3 * i + 1;
-    b_A[k] = A[k] - SOC * OCV;
-    k = 3 * i + 2;
-    b_A[k] = A[k] - d1 * OCV;
+    SOC = C_x[i];
+    b_A[3 * i] = A[3 * i] - d * SOC;
+    A_tmp = 3 * i + 1;
+    b_A[A_tmp] = A[A_tmp] - d1 * SOC;
+    A_tmp = 3 * i + 2;
+    b_A[A_tmp] = A[A_tmp] - d2 * SOC;
   }
 
   for (i = 0; i < 3; i++) {
     d = b_A[i];
-    SOC = b_A[i + 3];
-    d1 = b_A[i + 6];
+    d1 = b_A[i + 3];
+    d2 = b_A[i + 6];
     for (i1 = 0; i1 < 3; i1++) {
-      A[i + 3 * i1] = (d * P_x[3 * i1] + SOC * P_x[3 * i1 + 1]) + d1 * P_x[3 *
-        i1 + 2];
+      A[i + 3 * i1] = (d * P_x[3 * i1] + d1 * P_x[3 * i1 + 1]) + d2 * P_x[3 * i1
+        + 2];
     }
   }
 
@@ -220,48 +224,48 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
   }
 
   /*  Update */
-  OCV = 0.0;
+  SOC = 0.0;
   for (i = 0; i < 5; i++) {
     d = 0.0;
     for (i1 = 0; i1 < 5; i1++) {
       d += C_z[i1] * P_z[i1 + 5 * i];
     }
 
-    OCV += d * C_z[i];
+    SOC += d * C_z[i];
   }
 
-  OCV = 1.0 / (OCV + R_z);
+  SOC = 1.0 / (SOC + R_z);
   for (i = 0; i < 5; i++) {
     d = 0.0;
     for (i1 = 0; i1 < 5; i1++) {
       d += P_z[i + 5 * i1] * C_z[i1];
     }
 
-    d *= OCV;
+    d *= SOC;
     KalmanGain_z[i] = d;
-    Z[i] += d * Error_x;
+    Z[i] += d * *Error_x;
   }
 
   for (i = 0; i < 25; i++) {
     b_I[i] = 0;
   }
 
-  for (k = 0; k < 5; k++) {
-    b_I[k + 5 * k] = 1;
+  for (A_tmp = 0; A_tmp < 5; A_tmp++) {
+    b_I[A_tmp + 5 * A_tmp] = 1;
   }
 
   for (i = 0; i < 5; i++) {
     for (i1 = 0; i1 < 5; i1++) {
-      k = i1 + 5 * i;
-      d_I[k] = (double)b_I[k] - KalmanGain_z[i1] * C_z[i];
+      A_tmp = i1 + 5 * i;
+      d_I[A_tmp] = (double)b_I[A_tmp] - KalmanGain_z[i1] * C_z[i];
     }
   }
 
   for (i = 0; i < 5; i++) {
     for (i1 = 0; i1 < 5; i1++) {
       d = 0.0;
-      for (k = 0; k < 5; k++) {
-        d += d_I[i + 5 * k] * P_z[k + 5 * i1];
+      for (A_tmp = 0; A_tmp < 5; A_tmp++) {
+        d += d_I[i + 5 * A_tmp] * P_z[A_tmp + 5 * i1];
       }
 
       c_I[i + 5 * i1] = d;
@@ -269,9 +273,6 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
   }
 
   memcpy(&P_z[0], &c_I[0], 25U * sizeof(double));
-    //Serial.print(" ---> C code -- SOC = ");
-    //Serial.println(X[0]);
-  return Error_x;
 }
 
 /*
@@ -279,3 +280,4 @@ double extendedKalmanFilter(double U, double X[3], double Z[5], const double
  *
  * [EOF]
  */
+
