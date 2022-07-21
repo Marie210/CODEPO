4ad80887b612f3332c5511de015fbe73bbbe7aac
colinDietrich <44623378+colinDietrich@users.noreply.github.com>
2022-07-21 14:45:47 +0100

    Update extendedKalmanFilter.h

--- codes_arduino/libraries/extended_kalman_filter/extendedKalmanFilter.h
+++ codes_arduino/libraries/extended_kalman_filter/extendedKalmanFilter.h
@@ -5,7 +5,7 @@
  * File: extendedKalmanFilter.h
  *
  * MATLAB Coder version            : 5.1
- * C/C++ source code generated on  : 15-Feb-2022 16:12:22
+ * C/C++ source code generated on  : 21-Jul-2022 14:32:40
  */
 
 #ifndef EXTENDEDKALMANFILTER_H
@@ -22,10 +22,11 @@ extern "C" {
 #endif
 
   /* Function Declarations */
-  extern double extendedKalmanFilter(double U, double X[3], double Z[5], const
-    double SOCOCV[12], const double dSOCOCV[11], double Vt_Actual, double P_x[9],
+  extern void extendedKalmanFilter(double U, double X[3], double Z[5], const
+    double SOCOCV[5], const double dSOCOCV[4], double Vt_Actual, double P_x[9],
     double P_z[25], const double Q_x[9], const double Q_z[25], double R_x,
-    double R_z, double DeltaT, double Qn_rated);
+    double R_z, double DeltaT, double Qn_rated, double *Error_x, double
+    *TerminalVoltage);
 
 #ifdef __cplusplus
 
